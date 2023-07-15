13eb52a977a51cc0c1cf15e5d09c9b42c2dfb77c
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-07-14 22:05:41 -0300

    delete seller

--- SalesWebMvc/Services/Interfaces/ISellerService.cs
+++ SalesWebMvc/Services/Interfaces/ISellerService.cs
@@ -5,6 +5,8 @@ namespace SalesWebMvc.Services.Interfaces
     public interface ISellerService
     {
         List<Seller> FindAll();
+        Seller FindById(int id);
         void Insert(Seller obj);
+        void Remove(int id);
     }
 }
