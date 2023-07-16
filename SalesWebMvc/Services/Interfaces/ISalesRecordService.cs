0d89ba1f214e7d3d9efab969649ac812c0ff3219
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-07-16 15:32:12 -0300

    implementing grouping search

--- SalesWebMvc/Services/Interfaces/ISalesRecordService.cs
+++ SalesWebMvc/Services/Interfaces/ISalesRecordService.cs
@@ -5,5 +5,6 @@ namespace SalesWebMvc.Services.Interfaces
     public interface ISalesRecordService
     {
         Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate);
+        Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate);
     }
 }
